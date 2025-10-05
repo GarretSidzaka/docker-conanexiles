a466f0486e8a47a4642ec0119d6bb8dd2fec73c1
garret <garret@x9drwif.hole>
2025-10-04 23:38:53 -0700

    added adminer, added env

---
+++ sqlite.php
@@ -0,0 +1,5 @@
+
+<?php
+require("plugins/login-password-less.php");
+// TODO: inline the result of password_hash() so that the password is not visible in source codes
+return new AdminerLoginPasswordLess(password_hash("admin", PASSWORD_DEFAULT));
